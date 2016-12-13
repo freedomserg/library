45d741494b6e9d55d909f01c9d8c518996f53aad
freedomserg <freedomserg97@gmail.com>
2016-12-14 01:53:26 +0200

    prepared BookService class

---
+++ src/main/java/net/freedomserg/projects/library/service/BookService.java
@@ -0,0 +1,49 @@
+package net.freedomserg.projects.library.service;
+
+import net.freedomserg.projects.library.model.dao.BookDao;
+import net.freedomserg.projects.library.model.entity.Book;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class BookService {
+
+    private BookDao bookDao;
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Transactional
+    public void add(String bookName, String author) {
+        Book book = createNewBook(bookName, author);
+        bookDao.save(book);
+    }
+
+    @Transactional
+    public void remove(String bookName) {
+        bookDao.delete(bookName);
+    }
+
+    @Transactional
+    public void edit(Book book, String newBookName) {
+        bookDao.update(book, newBookName);
+    }
+
+    @Transactional
+    public List<Book> getByName(String bookName) {
+        return bookDao.loadByName(bookName);
+    }
+
+    @Transactional
+    public List<Book> getAll() {
+        return bookDao.loadAll();
+    }
+
+    private Book createNewBook(String bookName, String author) {
+        Book book = new Book();
+        book.setName(bookName);
+        book.setAuthor(author);
+        return book;
+    }
+}
