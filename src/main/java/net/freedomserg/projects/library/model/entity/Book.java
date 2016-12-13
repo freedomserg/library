f46092835723c780e152726dcf5886b188017200
freedomserg <freedomserg97@gmail.com>
2016-12-13 17:31:25 +0200

    prepared Book entity class

---
+++ src/main/java/net/freedomserg/projects/library/model/entity/Book.java
@@ -0,0 +1,78 @@
+package net.freedomserg.projects.library.model.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "book",
+        uniqueConstraints = {@UniqueConstraint(columnNames={"name", "author"})})
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "author")
+    @NotNull
+    private String author;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        if (!name.equals(book.name)) return false;
+        return author.equals(book.author);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + author.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
