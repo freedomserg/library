a12a380b8a6d7636f01ffe89bdca8b19876c0804
freedomserg <freedomserg97@gmail.com>
2016-12-14 00:47:30 +0200

    prepared MoreThanOnebookToModifyException class

---
+++ src/main/java/net/freedomserg/projects/library/exception/MoreThanOneBookToModifyException.java
@@ -0,0 +1,8 @@
+package net.freedomserg.projects.library.exception;
+
+public class MoreThanOneBookToModifyException extends LibraryException {
+
+    public MoreThanOneBookToModifyException(String message) {
+        super(message);
+    }
+}
