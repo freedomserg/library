6f0061bdb1e622e21ba5da2ba3f0b82bf1c9d1a5
freedomserg <freedomserg97@gmail.com>
2016-12-14 23:48:50 +0200

    started to implement DbExecutorService

--- src/main/java/net/freedomserg/projects/library/service/DbExecutorService.java
+++ src/main/java/net/freedomserg/projects/library/service/DbExecutorService.java
@@ -1,7 +1,8 @@
 package net.freedomserg.projects.library.service;
 
-import net.freedomserg.projects.library.userInteraction.AddingDbExecutor;
-import net.freedomserg.projects.library.userInteraction.DbExecutor;
+import net.freedomserg.projects.library.dbExecutors.AddDbExecutor;
+import net.freedomserg.projects.library.dbExecutors.DbExecutor;
+import net.freedomserg.projects.library.dbExecutors.RemoveDbExecutor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,16 +10,20 @@ import java.util.Queue;
 
 public class DbExecutorService {
 
-    public static DbExecutor getDbWorker(String operation, Queue params) {
+    public static DbExecutor getDbExecutor(String operation, Queue params) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml",
                 "hibernate-context.xml");
         switch(operation){
             case "add":
-                AddingDbExecutor dbWorker = applicationContext.getBean("addDbWorker", AddingDbExecutor.class);
-                dbWorker.setParams(params);
-                return dbWorker;
+                AddDbExecutor addingDbExecutor =
+                        applicationContext.getBean("addDbExecutor", AddDbExecutor.class);
+                addingDbExecutor.setParams(params);
+                return addingDbExecutor;
             case "remove":
-
+                RemoveDbExecutor removingDbExecutor =
+                        applicationContext.getBean("removeDbExecutor", RemoveDbExecutor.class);
+                removingDbExecutor.setParams(params);
+                return removingDbExecutor;
 
         }
 
