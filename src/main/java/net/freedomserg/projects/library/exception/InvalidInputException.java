319b06b0dd8fe1a8aa8027a1033d797e6f0d89ef
freedomserg <freedomserg97@gmail.com>
2016-12-14 23:46:31 +0200

    prepared InvalidInputException class

---
+++ src/main/java/net/freedomserg/projects/library/exception/InvalidInputException.java
@@ -0,0 +1,8 @@
+package net.freedomserg.projects.library.exception;
+
+public class InvalidInputException extends LibraryException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
