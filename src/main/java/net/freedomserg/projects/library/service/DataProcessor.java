dbcc6e397923d3ac166101f8b23f123654a72754
freedomserg <freedomserg97@gmail.com>
2016-12-14 23:48:00 +0200

    prepared DataProcessor class

--- src/main/java/net/freedomserg/projects/library/service/DataProcessor.java
+++ src/main/java/net/freedomserg/projects/library/service/DataProcessor.java
@@ -2,28 +2,21 @@ package net.freedomserg.projects.library.service;
 
 import net.freedomserg.projects.library.exception.InvalidInputException;
 import net.freedomserg.projects.library.dbExecutors.DbExecutor;
-import net.freedomserg.projects.library.service.DbExecutorService;
 
 import java.util.*;
 
 public class DataProcessor {
 
-    private String input;
-    private Queue<String> params;
-    private static List<String> operations = new ArrayList<>(Arrays.asList("add", "remove", "edit", "all books"));
+    private static List<String> validOperations = new ArrayList<>(Arrays.asList("add", "remove", "edit", "all books"));
 
-    public DataProcessor(String input) {
-        this.input = input;
-    }
-
-    public void process() {
+    public static void process(String input) {
         String[] inputWords = input.split(" ");
-        params = new ArrayDeque(Arrays.asList(inputWords));
+        Queue<String> params = new ArrayDeque(Arrays.asList(inputWords));
         String operation = params.poll();
         if (operation != null) {
-           if (operations.contains(operation.toLowerCase())) {
-               DbExecutor dbWorker = DbExecutorService.getDbExecutor(operation, params);
-               dbWorker.execute();
+           if (validOperations.contains(operation.toLowerCase())) {
+               DbExecutor dbExecutor = DbExecutorService.getDbExecutor(operation, params);
+               dbExecutor.execute();
            } else {
                 throw new InvalidInputException("Invalid input. Try again.");
            }
