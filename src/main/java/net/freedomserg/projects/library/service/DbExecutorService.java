e25c01e517f95db9d4f9c59950201fea1e7b385e
freedomserg <freedomserg97@gmail.com>
2016-12-15 20:33:50 +0200

    implemented logic regarding edit operation

--- src/main/java/net/freedomserg/projects/library/service/DbExecutorService.java
+++ src/main/java/net/freedomserg/projects/library/service/DbExecutorService.java
@@ -2,6 +2,7 @@ package net.freedomserg.projects.library.service;
 
 import net.freedomserg.projects.library.dbExecutors.AddDbExecutor;
 import net.freedomserg.projects.library.dbExecutors.DbExecutor;
+import net.freedomserg.projects.library.dbExecutors.EditDbExecutor;
 import net.freedomserg.projects.library.dbExecutors.RemoveDbExecutor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,16 +16,20 @@ public class DbExecutorService {
                 "hibernate-context.xml");
         switch(operation){
             case "add":
-                AddDbExecutor addingDbExecutor =
+                AddDbExecutor addDbExecutor =
                         applicationContext.getBean("addDbExecutor", AddDbExecutor.class);
-                addingDbExecutor.setParams(params);
-                return addingDbExecutor;
+                addDbExecutor.setParams(params);
+                return addDbExecutor;
             case "remove":
-                RemoveDbExecutor removingDbExecutor =
+                RemoveDbExecutor removeDbExecutor =
                         applicationContext.getBean("removeDbExecutor", RemoveDbExecutor.class);
-                removingDbExecutor.setParams(params);
-                return removingDbExecutor;
-
+                removeDbExecutor.setParams(params);
+                return removeDbExecutor;
+            case "edit":
+                EditDbExecutor editDbExecutor =
+                        applicationContext.getBean("editDbExecutor", EditDbExecutor.class);
+                editDbExecutor.setParams(params);
+                return editDbExecutor;
         }
 
         return null;
